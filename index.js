2fbfa3a112172d956a3f8893d58ea37b4fe3b013
Nour Majdoub <154745715+NoorMajdoub@users.noreply.github.com>
2024-12-15 07:55:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import Pipeline from "./components/pipeline/Pipeline";
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 
-// Material Dashboard 2 React example components
+
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
